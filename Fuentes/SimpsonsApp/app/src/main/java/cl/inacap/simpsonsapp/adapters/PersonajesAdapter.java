dd0db6389aa3fe76c62ced6e3a4445e2483d9090
Dino <dinoa.reyes@gmail.com>
2020-11-25 02:24:25 -0300

    Base y conexion entre fragment lista,falta conectar con el api y mejorarla visualmente

---
+++ Fuentes/SimpsonsApp/app/src/main/java/cl/inacap/simpsonsapp/adapters/PersonajesAdapter.java
@@ -0,0 +1,48 @@
+package cl.inacap.simpsonsapp.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import cl.inacap.simpsonsapp.R;
+import cl.inacap.simpsonsapp.dto.Personaje;
+
+public class PersonajesAdapter extends ArrayAdapter<Personaje> {
+    private List<Personaje> personajes;
+    private Activity activity;
+
+
+    public PersonajesAdapter(@NonNull Activity context, int resource, @NonNull List<Personaje> objects) {
+        super(context, resource, objects);
+        this.personajes = objects;
+        this.activity = context;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = this.activity.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.list_personajes,null,true);
+        TextView nombreTxt = rowView.findViewById(R.id.nombre_Txt);
+        ImageView imagePer = rowView.findViewById(R.id.image_personaje);
+        nombreTxt.setText(personajes.get(position).getCharacter());
+        Picasso.get().load(this.personajes.get(position).getImage()).resize(300,300).centerCrop().into(imagePer);
+        return rowView;
+
+
+
+    }
+}
